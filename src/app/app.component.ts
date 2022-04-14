983773e29b94a77e9ae61e712bd7c1590c9681d5
rakiafelly <rakiafelly14@gmail.com>
2022-04-14 14:13:58 +0100

    dynamicform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormArray, FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DynamicForm';
+  personalInformations: FormGroup;
+  submitted=false;
+  constructor() {
+    this.personalInformations = new FormGroup({
+      skills: new FormArray([]),
+      experienceProfessionel: new FormArray([]),
+    });
+  }
+  get skills(): FormArray {
+    return this.personalInformations.get('skills') as FormArray
+  }
+
+  addSkill() {
+    this.skills.push(new FormControl('',[Validators.required]))
+  }
+
+  deleteSkill(i: number) {
+    this.skills.removeAt(i)
+  }
+
+  get experienceProfessionel(): FormArray {
+    return this.personalInformations.get('experienceProfessionel') as FormArray
+  }
+
+  addExperience() {
+    this.experienceProfessionel.push(new FormControl('',[Validators.required]))
+  }
+
+  deleteExperience(i: number) {
+    this.experienceProfessionel.removeAt(i)
+  }
+
+  onSubmit(){
+    this.submitted=true;
+    console.log(this.personalInformations.value)
+    if(this.personalInformations.invalid){
+      return;
+    }
+    console.log('form is submittes succssefuly')
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +11,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(){}
+
+}
